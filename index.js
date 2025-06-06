c030b9a55a6bf563455c0e2db8238f06b94439e9
Md Jihad Hossain <mdjihadhossain793@gmail.com>
2025-06-06 22:48:09 +0600

    Initialize server-side application with Express and MongoDB setup

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient } = require("mongodb");
+const port = 3000;
+
+// config dotenv
+require("dotenv").config();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// mongoDB connection string
+const mongoURI =
+    `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.ya0qxn8.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// mongodb client
+const client = new MongoClient(mongoURI);
+
+async function run() {
+    try {
+        const db = client.db("car-rental");
+        const carsCollection = db.collection("cars");
+
+        // Get all cars
+        app.get("/cars", async (req, res) => {
+            const result = await carsCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        // Start the server
+        app.listen(port, () => {
+            console.log(`Server running at http://localhost:${port}`);
+        });
+    } catch (error) {
+        console.log(`Error connecting to MongoDB: ${error.message}`);
+    }
+}
+
+run().catch((err) => {
+    console.dir(err);
+});
